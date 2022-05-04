b02e458969841dc91e760b815580938d5be6f700
Yichuan Shen <shen.yichuan@gmail.com>
2022-05-04 22:33:26 +0200

    Add HTML attributes

--- src/props/attr.rs
+++ src/props/attr.rs
@@ -68,3 +68,154 @@ impl From<Attr> for JsValue {
     attr.0.into()
   }
 }
+
+macro_rules! impl_attr {
+  ($($attr:ident, $attr_str:expr, $value:ty);*;) => {
+    $(
+      pub fn $attr(self, value: $value) -> Self {
+        self.insert($attr_str, value)
+      }
+    )*
+  };
+}
+
+impl Attr {
+  impl_attr! {
+    // Standard HTML Attributes
+    access_key, "accessKey", &str;
+    content_editable, "contentEditable", bool;
+    context_menu, "contextMenu", &str;
+    dir, "dir", &str;
+    draggable, "draggable", bool;
+    hidden, "hidden", bool;
+    id, "id", &str;
+    lang, "lang", &str;
+    placeholder, "placeholder", &str;
+    slot, "slot", &str;
+    spell_check, "spellCheck", bool;
+    tab_index, "tabIndex", i32;
+    title, "title", &str;
+    translate, "translate", &str;
+    radio_group, "radioGroup", &str;
+
+    // WAI-ARIA
+    role, "role", &str;
+
+    // RDFa Attributes
+    about, "about", &str;
+    datatype, "datatype", &str;
+    inlist, "inlist", impl Into<JsValue>;
+    prefix, "prefix", &str;
+    property, "property", &str;
+    resource, "resource", &str;
+    vocab, "vocab", &str;
+
+    // Living Standard
+    input_mode, "inputMode", &str;
+    is, "is", &str;
+
+    // Standard HTML Attributes
+    accept, "accept", &str;
+    accept_charset, "acceptCharset", &str;
+    action, "action", &str;
+    allow_full_screen, "allowFullScreen", bool;
+    allow_transparency, "allowTransparency", bool;
+    alt, "alt", &str;
+    auto_complete, "autoComplete", &str;
+    auto_focus, "autoFocus", bool;
+    auto_play, "autoPlay", bool;
+    capture, "capture", impl Into<JsValue>;
+    cell_padding, "cellPadding", impl Into<JsValue>;
+    cell_spacing, "cellSpacing", impl Into<JsValue>;
+    char_set, "charSet", &str;
+    challenge, "challenge", &str;
+    checked, "checked", bool;
+    cite, "cite", &str;
+    class_id, "classID", &str;
+    cols, "cols", u32;
+    col_span, "colSpan", u32;
+    content, "content", &str;
+    controls, "controls", bool;
+    coords, "coords", &str;
+    cross_origin, "crossOrigin", &str;
+    data, "data", &str;
+    date_time, "dateTime", &str;
+    default, "default", bool;
+    defer, "defer", bool;
+    disabled, "disabled", bool;
+    download, "download", impl Into<JsValue>;
+    enc_type, "encType", &str;
+    form, "form", &str;
+    form_action, "formAction", &str;
+    form_enc_type, "formEncType", &str;
+    form_method, "formMethod", &str;
+    form_no_validate, "formNoValidate", bool;
+    form_target, "formTarget", &str;
+    frame_border, "frameBorder", impl Into<JsValue>;
+    headers, "headers", &str;
+    height, "height", impl Into<JsValue>;
+    high, "high", f64;
+    href, "href", &str;
+    href_lang, "hrefLang", &str;
+    html_for, "htmlFor", &str;
+    http_equiv, "httpEquiv", &str;
+    integrity, "integrity", &str;
+    key_params, "keyParams", &str;
+    key_type, "keyType", &str;
+    kind, "kind", &str;
+    label, "label", &str;
+    list, "list", &str;
+    low, "low", f64;
+    manifest, "manifest", &str;
+    margin_height, "marginHeight", f64;
+    margin_width, "marginWidth", f64;
+    max, "max", f64;
+    max_length, "maxLength", f64;
+    media, "media", &str;
+    media_group, "mediaGroup", &str;
+    method, "method", &str;
+    min, "min", impl Into<JsValue>;
+    min_length, "minLength", f64;
+    multiple, "multiple", bool;
+    muted, "muted", bool;
+    name, "name", &str;
+    nonce, "nonce", &str;
+    no_validate, "noValidate", bool;
+    open, "open", bool;
+    optimum, "optimum", f64;
+    pattern, "pattern", &str;
+    plays_inline, "playsInline", bool;
+    poster, "poster", &str;
+    preload, "preload", &str;
+    read_only, "readOnly", bool;
+    rel, "rel", &str;
+    required, "required", bool;
+    reversed, "reversed", bool;
+    rows, "rows", u32;
+    row_span, "rowSpan", u32;
+    sandbox, "sandbox", &str;
+    scope, "scope", &str;
+    scoped, "scoped", bool;
+    scrolling, "scrolling", &str;
+    seamless, "seamless", bool;
+    selected, "selected", bool;
+    shape, "shape", &str;
+    size, "size", f64;
+    sizes, "sizes", &str;
+    span, "span", u32;
+    src, "src", &str;
+    src_doc, "srcDoc", &str;
+    src_lang, "srcLang", &str;
+    src_set, "srcSet", &str;
+    start, "start", f64;
+    step, "step", impl Into<JsValue>;
+    summary, "summary", &str;
+    target, "target", &str;
+    typ, "type", &str;
+    use_map, "useMap", &str;
+    value, "value", impl Into<JsValue>;
+    width, "width", impl Into<JsValue>;
+    wmode, "wmode", &str;
+    wrap, "wrap", &str;
+  }
+}
--- src/props/event.rs
+++ src/props/event.rs
@@ -1,7 +1,11 @@
 use super::Attr;
+use web_sys::{
+  AnimationEvent, DragEvent, Event, FocusEvent, KeyboardEvent, MouseEvent,
+  PointerEvent, TransitionEvent, UiEvent, WheelEvent,
+};
 
 macro_rules! impl_event {
-  ($($on_event:ident, $on_event_str:expr, $evt:path);*;) => {
+  ($($on_event:ident, $on_event_str:expr, $evt:ty);*;) => {
     $(
       pub fn $on_event(self, f: impl Fn($evt) + 'static) -> Self {
         self.insert_callback($on_event_str, f)
@@ -12,107 +16,152 @@ macro_rules! impl_event {
 
 impl Attr {
   impl_event! {
-    on_focus, "onFocus", web_sys::FocusEvent;
-    on_focus_capture, "onFocusCapture", web_sys::FocusEvent;
-    on_blur, "onBlur", web_sys::FocusEvent;
-    on_blur_capture, "onBlurCapture", web_sys::FocusEvent;
+    on_focus, "onFocus", FocusEvent;
+    on_focus_capture, "onFocusCapture", FocusEvent;
+    on_blur, "onBlur", FocusEvent;
+    on_blur_capture, "onBlurCapture", FocusEvent;
 
-    on_change, "onChange", web_sys::Event;
-    on_change_capture, "onChangeCapture", web_sys::Event;
-    on_before_input, "onBeforeInput", web_sys::Event;
-    on_before_input_capture, "onBeforeInputCapture", web_sys::Event;
-    on_input, "onInput", web_sys::Event;
-    on_input_capture, "onInputCapture", web_sys::Event;
-    on_reset, "onReset", web_sys::Event;
-    on_reset_capture, "onResetCapture", web_sys::Event;
-    on_submit, "onSubmit", web_sys::Event;
-    on_submit_capture, "onSubmitCapture", web_sys::Event;
-    on_invalid, "onInvalid", web_sys::Event;
-    on_invalid_capture, "onInvalidCapture", web_sys::Event;
-    on_select, "onSelect", web_sys::UiEvent;
-    on_select_capture, "onSelectCapture", web_sys::UiEvent;
+    on_change, "onChange", Event;
+    on_change_capture, "onChangeCapture", Event;
+    on_before_input, "onBeforeInput", Event;
+    on_before_input_capture, "onBeforeInputCapture", Event;
+    on_input, "onInput", Event;
+    on_input_capture, "onInputCapture", Event;
+    on_reset, "onReset", Event;
+    on_reset_capture, "onResetCapture", Event;
+    on_submit, "onSubmit", Event;
+    on_submit_capture, "onSubmitCapture", Event;
+    on_invalid, "onInvalid", Event;
+    on_invalid_capture, "onInvalidCapture", Event;
+    on_select, "onSelect", UiEvent;
+    on_select_capture, "onSelectCapture", UiEvent;
 
-    on_load, "onLoad", web_sys::Event;
-    on_load_capture, "onLoadCapture", web_sys::Event;
+    on_load, "onLoad", Event;
+    on_load_capture, "onLoadCapture", Event;
 
-    on_key_down, "onKeyDown", web_sys::KeyboardEvent;
-    on_key_down_capture, "onKeyDownCapture", web_sys::KeyboardEvent;
-    on_key_press, "onKeyPress", web_sys::KeyboardEvent;
-    on_key_press_capture, "onKeyPressCapture", web_sys::KeyboardEvent;
-    on_key_up, "onKeyUp", web_sys::KeyboardEvent;
-    on_key_up_capture, "onKeyUpCapture", web_sys::KeyboardEvent;
+    on_key_down, "onKeyDown", KeyboardEvent;
+    on_key_down_capture, "onKeyDownCapture", KeyboardEvent;
+    on_key_press, "onKeyPress", KeyboardEvent;
+    on_key_press_capture, "onKeyPressCapture", KeyboardEvent;
+    on_key_up, "onKeyUp", KeyboardEvent;
+    on_key_up_capture, "onKeyUpCapture", KeyboardEvent;
 
-    on_aux_click, "onAuxClick", web_sys::MouseEvent;
-    on_aux_click_capture, "onAuxClickCapture", web_sys::MouseEvent;
-    on_click, "onClick", web_sys::MouseEvent;
-    on_click_capture, "onClickCapture", web_sys::MouseEvent;
-    on_context_menu, "onContextMenu", web_sys::MouseEvent;
-    on_context_menu_capture, "onContextMenuCapture", web_sys::MouseEvent;
-    on_double_click, "onDoubleClick", web_sys::MouseEvent;
-    on_double_click_capture, "onDoubleClickCapture", web_sys::MouseEvent;
-    on_mouse_down, "onMouseDown", web_sys::MouseEvent;
-    on_mouse_down_capture, "onMouseDownCapture", web_sys::MouseEvent;
-    on_mouse_enter, "onMouseEnter", web_sys::MouseEvent;
-    on_mouse_leave, "onMouseLeave", web_sys::MouseEvent;
-    on_mouse_move, "onMouseMove", web_sys::MouseEvent;
-    on_mouse_move_capture, "onMouseMoveCapture", web_sys::MouseEvent;
-    on_mouse_out, "onMouseOut", web_sys::MouseEvent;
-    on_mouse_out_capture, "onMouseOutCapture", web_sys::MouseEvent;
-    on_mouse_over, "onMouseOver", web_sys::MouseEvent;
-    on_mouse_over_capture, "onMouseOverCapture", web_sys::MouseEvent;
-    on_mouse_up, "onMouseUp", web_sys::MouseEvent;
-    on_mouse_up_capture, "onMouseUpCapture", web_sys::MouseEvent;
+    on_abort, "onAbort", Event;
+    on_abort_capture, "onAbortCapture", Event;
+    on_can_play, "onCanPlay", Event;
+    on_can_play_capture, "onCanPlayCapture", Event;
+    on_can_play_through, "onCanPlayThrough", Event;
+    on_can_play_through_capture, "onCanPlayThroughCapture", Event;
+    on_duration_change, "onDurationChange", Event;
+    on_duration_change_capture, "onDurationChangeCapture", Event;
+    on_emptied, "onEmptied", Event;
+    on_emptied_capture, "onEmptiedCapture", Event;
+    on_encrypted, "onEncrypted", Event;
+    on_encrypted_capture, "onEncryptedCapture", Event;
+    on_ended, "onEnded", Event;
+    on_ended_capture, "onEndedCapture", Event;
+    on_loaded_data, "onLoadedData", Event;
+    on_loaded_data_capture, "onLoadedDataCapture", Event;
+    on_loaded_metadata, "onLoadedMetadata", Event;
+    on_loaded_metadata_capture, "onLoadedMetadataCapture", Event;
+    on_load_start, "onLoadStart", Event;
+    on_load_start_capture, "onLoadStartCapture", Event;
+    on_pause, "onPause", Event;
+    on_pause_capture, "onPauseCapture", Event;
+    on_play, "onPlay", Event;
+    on_play_capture, "onPlayCapture", Event;
+    on_playing, "onPlaying", Event;
+    on_playing_capture, "onPlayingCapture", Event;
+    on_progress, "onProgress", Event;
+    on_progress_capture, "onProgressCapture", Event;
+    on_rate_change, "onRateChange", Event;
+    on_rate_change_capture, "onRateChangeCapture", Event;
+    on_seeked, "onSeeked", Event;
+    on_seeked_capture, "onSeekedCapture", Event;
+    on_seeking, "onSeeking", Event;
+    on_seeking_capture, "onSeekingCapture", Event;
+    on_stalled, "onStalled", Event;
+    on_stalled_capture, "onStalledCapture", Event;
+    on_suspend, "onSuspend", Event;
+    on_suspend_capture, "onSuspendCapture", Event;
+    on_time_update, "onTimeUpdate", Event;
+    on_time_update_capture, "onTimeUpdateCapture", Event;
+    on_volume_change, "onVolumeChange", Event;
+    on_volume_change_capture, "onVolumeChangeCapture", Event;
+    on_waiting, "onWaiting", Event;
+    on_waiting_capture, "onWaitingCapture", Event;
 
-    on_pointer_down, "onPointerDown", web_sys::PointerEvent;
-    on_pointer_down_capture, "onPointerDownCapture", web_sys::PointerEvent;
-    on_pointer_move, "onPointerMove", web_sys::PointerEvent;
-    on_pointer_move_capture, "onPointerMoveCapture", web_sys::PointerEvent;
-    on_pointer_up, "onPointerUp", web_sys::PointerEvent;
-    on_pointer_up_capture, "onPointerUpCapture", web_sys::PointerEvent;
-    on_pointer_cancel, "onPointerCancel", web_sys::PointerEvent;
-    on_pointer_cancel_capture, "onPointerCancelCapture", web_sys::PointerEvent;
-    on_pointer_enter, "onPointerEnter", web_sys::PointerEvent;
-    on_pointer_enter_capture, "onPointerEnterCapture", web_sys::PointerEvent;
-    on_pointer_leave, "onPointerLeave", web_sys::PointerEvent;
-    on_pointer_leave_capture, "onPointerLeaveCapture", web_sys::PointerEvent;
-    on_pointer_over, "onPointerOver", web_sys::PointerEvent;
-    on_pointer_over_capture, "onPointerOverCapture", web_sys::PointerEvent;
-    on_pointer_out, "onPointerOut", web_sys::PointerEvent;
-    on_pointer_out_capture, "onPointerOutCapture", web_sys::PointerEvent;
-    on_got_pointer_capture, "onGotPointerCapture", web_sys::PointerEvent;
-    on_got_pointer_capture_capture, "onGotPointerCaptureCapture", web_sys::PointerEvent;
-    on_lost_pointer_capture, "onLostPointerCapture", web_sys::PointerEvent;
-    on_lost_pointer_capture_capture, "onLostPointerCaptureCapture", web_sys::PointerEvent;
+    on_aux_click, "onAuxClick", MouseEvent;
+    on_aux_click_capture, "onAuxClickCapture", MouseEvent;
+    on_click, "onClick", MouseEvent;
+    on_click_capture, "onClickCapture", MouseEvent;
+    on_context_menu, "onContextMenu", MouseEvent;
+    on_context_menu_capture, "onContextMenuCapture", MouseEvent;
+    on_double_click, "onDoubleClick", MouseEvent;
+    on_double_click_capture, "onDoubleClickCapture", MouseEvent;
+    on_mouse_down, "onMouseDown", MouseEvent;
+    on_mouse_down_capture, "onMouseDownCapture", MouseEvent;
+    on_mouse_enter, "onMouseEnter", MouseEvent;
+    on_mouse_leave, "onMouseLeave", MouseEvent;
+    on_mouse_move, "onMouseMove", MouseEvent;
+    on_mouse_move_capture, "onMouseMoveCapture", MouseEvent;
+    on_mouse_out, "onMouseOut", MouseEvent;
+    on_mouse_out_capture, "onMouseOutCapture", MouseEvent;
+    on_mouse_over, "onMouseOver", MouseEvent;
+    on_mouse_over_capture, "onMouseOverCapture", MouseEvent;
+    on_mouse_up, "onMouseUp", MouseEvent;
+    on_mouse_up_capture, "onMouseUpCapture", MouseEvent;
 
-    on_drag, "onDrag", web_sys::DragEvent;
-    on_drag_capture, "onDragCapture", web_sys::DragEvent;
-    on_drag_end, "onDragEnd", web_sys::DragEvent;
-    on_drag_end_capture, "onDragEndCapture", web_sys::DragEvent;
-    on_drag_enter, "onDragEnter", web_sys::DragEvent;
-    on_drag_enter_capture, "onDragEnterCapture", web_sys::DragEvent;
-    on_drag_exit, "onDragExit", web_sys::DragEvent;
-    on_drag_exit_capture, "onDragExitCapture", web_sys::DragEvent;
-    on_drag_leave, "onDragLeave", web_sys::DragEvent;
-    on_drag_leave_capture, "onDragLeaveCapture", web_sys::DragEvent;
-    on_drag_over, "onDragOver", web_sys::DragEvent;
-    on_drag_over_capture, "onDragOverCapture", web_sys::DragEvent;
-    on_drag_start, "onDragStart", web_sys::DragEvent;
-    on_drag_start_capture, "onDragStartCapture", web_sys::DragEvent;
-    on_drop, "onDrop", web_sys::DragEvent;
-    on_drop_capture, "onDropCapture", web_sys::DragEvent;
+    on_pointer_down, "onPointerDown", PointerEvent;
+    on_pointer_down_capture, "onPointerDownCapture", PointerEvent;
+    on_pointer_move, "onPointerMove", PointerEvent;
+    on_pointer_move_capture, "onPointerMoveCapture", PointerEvent;
+    on_pointer_up, "onPointerUp", PointerEvent;
+    on_pointer_up_capture, "onPointerUpCapture", PointerEvent;
+    on_pointer_cancel, "onPointerCancel", PointerEvent;
+    on_pointer_cancel_capture, "onPointerCancelCapture", PointerEvent;
+    on_pointer_enter, "onPointerEnter", PointerEvent;
+    on_pointer_enter_capture, "onPointerEnterCapture", PointerEvent;
+    on_pointer_leave, "onPointerLeave", PointerEvent;
+    on_pointer_leave_capture, "onPointerLeaveCapture", PointerEvent;
+    on_pointer_over, "onPointerOver", PointerEvent;
+    on_pointer_over_capture, "onPointerOverCapture", PointerEvent;
+    on_pointer_out, "onPointerOut", PointerEvent;
+    on_pointer_out_capture, "onPointerOutCapture", PointerEvent;
+    on_got_pointer_capture, "onGotPointerCapture", PointerEvent;
+    on_got_pointer_capture_capture, "onGotPointerCaptureCapture", PointerEvent;
+    on_lost_pointer_capture, "onLostPointerCapture", PointerEvent;
+    on_lost_pointer_capture_capture, "onLostPointerCaptureCapture", PointerEvent;
 
-    on_scroll, "onScroll", web_sys::UiEvent;
-    on_scroll_capture, "onScrollCapture", web_sys::UiEvent;
-    on_wheel, "onWheel", web_sys::WheelEvent;
-    on_wheel_capture, "onWheelCapture", web_sys::WheelEvent;
+    on_drag, "onDrag", DragEvent;
+    on_drag_capture, "onDragCapture", DragEvent;
+    on_drag_end, "onDragEnd", DragEvent;
+    on_drag_end_capture, "onDragEndCapture", DragEvent;
+    on_drag_enter, "onDragEnter", DragEvent;
+    on_drag_enter_capture, "onDragEnterCapture", DragEvent;
+    on_drag_exit, "onDragExit", DragEvent;
+    on_drag_exit_capture, "onDragExitCapture", DragEvent;
+    on_drag_leave, "onDragLeave", DragEvent;
+    on_drag_leave_capture, "onDragLeaveCapture", DragEvent;
+    on_drag_over, "onDragOver", DragEvent;
+    on_drag_over_capture, "onDragOverCapture", DragEvent;
+    on_drag_start, "onDragStart", DragEvent;
+    on_drag_start_capture, "onDragStartCapture", DragEvent;
+    on_drop, "onDrop", DragEvent;
+    on_drop_capture, "onDropCapture", DragEvent;
 
-    on_animation_start, "onAnimationStart", web_sys::AnimationEvent;
-    on_animation_start_capture, "onAnimationStartCapture", web_sys::AnimationEvent;
-    on_animation_end, "onAnimationEnd", web_sys::AnimationEvent;
-    on_animation_end_capture, "onAnimationEndCapture", web_sys::AnimationEvent;
-    on_animation_iteration, "onAnimationIteration", web_sys::AnimationEvent;
-    on_animation_iteration_capture, "onAnimationIterationCapture", web_sys::AnimationEvent;
-    on_transition_end, "onTransitionEnd", web_sys::TransitionEvent;
-    on_transition_end_capture, "onTransitionEndCapture", web_sys::TransitionEvent;
+    on_scroll, "onScroll", UiEvent;
+    on_scroll_capture, "onScrollCapture", UiEvent;
+    on_wheel, "onWheel", WheelEvent;
+    on_wheel_capture, "onWheelCapture", WheelEvent;
+
+    on_animation_start, "onAnimationStart", AnimationEvent;
+    on_animation_start_capture, "onAnimationStartCapture", AnimationEvent;
+    on_animation_end, "onAnimationEnd", AnimationEvent;
+    on_animation_end_capture, "onAnimationEndCapture", AnimationEvent;
+    on_animation_iteration, "onAnimationIteration", AnimationEvent;
+    on_animation_iteration_capture, "onAnimationIterationCapture", AnimationEvent;
+    on_transition_end, "onTransitionEnd", TransitionEvent;
+    on_transition_end_capture, "onTransitionEndCapture", TransitionEvent;
   }
 }
