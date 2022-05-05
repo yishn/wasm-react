5d640a827c46c1d1614aac1a17eaca2a636e52c3
Yichuan Shen <shen.yichuan@gmail.com>
2022-05-06 01:12:56 +0200

    Use Rc for cloning closures

--- src/hooks/use_effect.rs
+++ src/hooks/use_effect.rs
@@ -1,5 +1,5 @@
 use super::Deps;
-use crate::{react_bindings, Callback};
+use crate::{callback::Void, react_bindings, Callback};
 use wasm_bindgen::JsValue;
 
 pub fn use_effect<F, G>(f: F, deps: Deps)
@@ -8,9 +8,9 @@ where
   G: Fn() + 'static,
 {
   react_bindings::use_effect(
-    Callback::new(move |_: JsValue| -> JsValue {
+    Callback::new(move |_: Void| -> JsValue {
       let g = f();
-      Callback::new(move |_: JsValue| g()).into()
+      Callback::new(move |_: Void| g()).into()
     })
     .into(),
     deps.into(),
--- src/hooks/use_memo.rs
+++ src/hooks/use_memo.rs
@@ -1,5 +1,5 @@
 use super::{use_effect, use_state, Deps, UseState};
-use std::{fmt::Debug, ops::Deref};
+use std::{fmt::Debug, ops::Deref, rc::Rc};
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct UseMemo<T>(UseState<T>);
@@ -19,20 +19,21 @@ impl<T: Debug> Debug for UseMemo<T> {
 }
 
 pub fn use_memo<T>(
-  f: impl Fn() -> T + Clone + 'static,
+  f: impl Fn() -> T + 'static,
   deps: Deps,
 ) -> UseMemo<T>
 where
   T: 'static,
 {
-  let memo = use_state(|| f());
+  let rc_f = Rc::new(f);
+  let memo = use_state(|| rc_f());
 
   use_effect(
     {
       let memo = memo.clone();
 
       move || {
-        let f = f.clone();
+        let f = rc_f.clone();
         memo.update(move |memo| *memo = f());
         || ()
       }
