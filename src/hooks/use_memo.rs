bcc5403cd131aac8fa362c1520d473e35c0f054a
Yichuan Shen <shen.yichuan@gmail.com>
2022-05-07 12:45:50 +0200

    Fix use_memo()

--- src/hooks/use_memo.rs
+++ src/hooks/use_memo.rs
@@ -1,19 +1,20 @@
 use super::{use_ref, Deps, UseRef};
-use std::{fmt::Debug, ops::Deref, rc::Rc};
+use std::{fmt::Debug, ops::Deref};
+use wasm_bindgen::UnwrapThrowExt;
 
-pub struct UseMemo<T, D: PartialEq>(UseRef<(T, Deps<D>)>);
+pub struct UseMemo<T, D: PartialEq>(UseRef<Option<(T, Deps<D>)>>);
 
 impl<T, D: PartialEq> Deref for UseMemo<T, D> {
   type Target = T;
 
   fn deref(&self) -> &Self::Target {
-    &self.0.deref().current.0
+    &self.0.deref().current.as_ref().unwrap_throw().0
   }
 }
 
 impl<T: Debug, D: PartialEq> Debug for UseMemo<T, D> {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    self.0.deref().current.0.fmt(f)
+    self.0.deref().current.as_ref().unwrap_throw().0.fmt(f)
   }
 }
 
@@ -25,12 +26,11 @@ where
   T: 'static,
   D: PartialEq + 'static,
 {
-  let f = Rc::new(f);
-  let mut memo = use_ref((f(), deps.clone()));
+  let mut memo = use_ref(None::<(T, Deps<D>)>);
 
-  let old_deps = &memo.current.1;
-  if deps == Deps::All || &deps != old_deps {
-    memo.current = (f(), deps);
+  let old_deps = memo.current.as_ref().map(|memo| &memo.1);
+  if deps == Deps::All || Some(&deps) != old_deps {
+    memo.current = Some((f(), deps));
   }
 
   UseMemo(memo)
