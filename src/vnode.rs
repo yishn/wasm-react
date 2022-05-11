6790e6a99c27b5b49cbad10ca53e9b9a42fef8ae
Yichuan Shen <shen.yichuan@gmail.com>
2022-05-12 01:12:04 +0200

    Noone cares about char

--- src/vnode.rs
+++ src/vnode.rs
@@ -58,7 +58,7 @@ macro_rules! impl_into_vnode {
     $(
       impl From<$T> for VNode {
         fn from(value: $T) -> Self {
-          VNode(value.to_string().into())
+          VNode(value.into())
         }
       }
     )*
@@ -67,7 +67,7 @@ macro_rules! impl_into_vnode {
 
 // Implement `Into<VNode>` for as many `Display` types as possible
 impl_into_vnode! {
-  String, char,
+  String,
   f32, f64,
   i8, i16, i32, i64, i128, isize,
   u8, u16, u32, u64, u128, usize,
