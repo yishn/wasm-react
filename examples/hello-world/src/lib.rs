197f0e0bc4528126c86af5c23ee2d17d4f982b48
Yichuan Shen <shen.yichuan@gmail.com>
2022-05-13 01:51:36 +0200

    More complex hello world

--- examples/hello-world/src/lib.rs
+++ examples/hello-world/src/lib.rs
@@ -1,19 +1,28 @@
+use js_sys::Reflect;
 use wasm_bindgen::JsValue;
 use wasm_react::{c, export_component, h, Component, VNode};
 
-pub struct App;
+pub struct App {
+  name: Option<String>,
+}
 
 impl TryFrom<JsValue> for App {
   type Error = JsValue;
 
-  fn try_from(_: JsValue) -> Result<Self, Self::Error> {
-    Ok(App)
+  fn try_from(value: JsValue) -> Result<Self, Self::Error> {
+    Ok(App {
+      name: Reflect::get(&value, &"name".into())?.as_string(),
+    })
   }
 }
 
 impl Component for App {
   fn render(&self) -> VNode {
-    h!(h1).build(c!["Hello World!"])
+    h!(h1).build(c![if let Some(name) = self.name.as_ref() {
+      format!("Hello {}!", name)
+    } else {
+      "Hello World!".to_string()
+    }])
   }
 }
 
