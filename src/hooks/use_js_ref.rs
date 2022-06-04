dc3ef2032b445142c64c6596cf0914acfbcf1e7a
Yichuan Shen <shen.yichuan@gmail.com>
2022-06-05 00:44:33 +0200

    Move use_js_ref into own file

---
+++ src/hooks/use_js_ref.rs
@@ -0,0 +1,91 @@
+use crate::{react_bindings, Persisted, PersistedOrigin};
+use js_sys::Reflect;
+use std::{fmt::Debug, marker::PhantomData};
+use wasm_bindgen::{JsCast, JsValue, UnwrapThrowExt};
+
+/// Allows access to the underlying JS data persisted with [`use_js_ref()`].
+pub struct JsRefContainer<T>(JsValue, PhantomData<T>);
+
+impl<T: JsCast> JsRefContainer<T> {
+  /// Returns the underlying typed JS data.
+  pub fn current(&self) -> Option<T> {
+    self.current_untyped().dyn_into::<T>().ok()
+  }
+
+  /// Returns the underlying JS data as [`JsValue`].
+  pub fn current_untyped(&self) -> JsValue {
+    Reflect::get(&self.0, &"current".into()).unwrap_throw()
+  }
+
+  /// Sets the underlying JS data.
+  pub fn set_current(&self, value: Option<&T>) {
+    Reflect::set(
+      &self.0,
+      &"current".into(),
+      value.map(|t| t.as_ref()).unwrap_or(&JsValue::null()),
+    )
+    .unwrap_throw();
+  }
+}
+
+impl<T> Persisted for JsRefContainer<T> {
+  fn ptr(&self) -> PersistedOrigin {
+    PersistedOrigin
+  }
+}
+
+impl<T> Debug for JsRefContainer<T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.debug_tuple("JsRefContainer").field(&self.0).finish()
+  }
+}
+
+impl<T> Clone for JsRefContainer<T> {
+  fn clone(&self) -> Self {
+    Self(self.0.clone(), PhantomData)
+  }
+}
+
+impl<T> AsRef<JsValue> for JsRefContainer<T> {
+  fn as_ref(&self) -> &JsValue {
+    &self.0
+  }
+}
+
+impl<T> From<JsRefContainer<T>> for JsValue {
+  fn from(value: JsRefContainer<T>) -> Self {
+    value.0
+  }
+}
+
+/// This hook can persist JS data through the entire lifetime of the component.
+///
+/// Use this if you need JS to set the ref value. If you only need to mutate the
+/// data from Rust, use [`use_ref()`] instead.
+///
+/// # Example
+///
+/// ```
+/// # use wasm_react::{*, hooks::*};
+/// # struct MyComponent;
+/// impl Component for MyComponent {
+///   fn render(&self) -> VNode {
+///     let input_element = use_js_ref(None);
+///
+///     h!(div)
+///       .build(c![
+///         h!(input)
+///           .ref_container(&input_element)
+///           .html_type("text")
+///           .build(c![])
+///       ])
+///   }
+/// }
+/// ```
+pub fn use_js_ref<T: JsCast>(init: Option<T>) -> JsRefContainer<T> {
+  let ref_container = react_bindings::use_ref(
+    &init.map(|init| init.into()).unwrap_or(JsValue::null()),
+  );
+
+  JsRefContainer(ref_container, PhantomData)
+}
--- src/hooks/use_ref.rs
+++ src/hooks/use_ref.rs
@@ -3,11 +3,10 @@ use js_sys::Reflect;
 use std::{
   cell::{Ref, RefCell, RefMut},
   fmt::Debug,
-  marker::PhantomData,
   mem::ManuallyDrop,
   rc::Rc,
 };
-use wasm_bindgen::{prelude::Closure, JsCast, JsValue, UnwrapThrowExt};
+use wasm_bindgen::{prelude::Closure, JsValue, UnwrapThrowExt};
 
 /// Allows access to the underlying data persisted with [`use_ref()`].
 ///
@@ -147,91 +146,4 @@ pub fn use_ref<T: 'static>(init: T) -> RefContainer<T> {
   );
 
   unsafe { RefContainer::try_from_js(&js_ref).unwrap_throw() }
-}
-
-/// Allows access to the underlying JS data persisted with [`use_js_ref()`].
-pub struct JsRefContainer<T>(JsValue, PhantomData<T>);
-
-impl<T: JsCast> JsRefContainer<T> {
-  /// Returns the underlying typed JS data.
-  pub fn current(&self) -> Option<T> {
-    self.current_untyped().dyn_into::<T>().ok()
-  }
-
-  /// Returns the underlying JS data as [`JsValue`].
-  pub fn current_untyped(&self) -> JsValue {
-    Reflect::get(&self.0, &"current".into()).unwrap_throw()
-  }
-
-  /// Sets the underlying JS data.
-  pub fn set_current(&self, value: Option<&T>) {
-    Reflect::set(
-      &self.0,
-      &"current".into(),
-      value.map(|t| t.as_ref()).unwrap_or(&JsValue::null()),
-    )
-    .unwrap_throw();
-  }
-}
-
-impl<T> Persisted for JsRefContainer<T> {
-  fn ptr(&self) -> PersistedOrigin {
-    PersistedOrigin
-  }
-}
-
-impl<T> Debug for JsRefContainer<T> {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    f.debug_tuple("JsRefContainer").field(&self.0).finish()
-  }
-}
-
-impl<T> Clone for JsRefContainer<T> {
-  fn clone(&self) -> Self {
-    Self(self.0.clone(), PhantomData)
-  }
-}
-
-impl<T> AsRef<JsValue> for JsRefContainer<T> {
-  fn as_ref(&self) -> &JsValue {
-    &self.0
-  }
-}
-
-impl<T> From<JsRefContainer<T>> for JsValue {
-  fn from(value: JsRefContainer<T>) -> Self {
-    value.0
-  }
-}
-
-/// This hook can persist JS data through the entire lifetime of the component.
-///
-/// Use this if you need JS to set the ref value. If you only need to mutate the
-/// data from Rust, use [`use_ref()`] instead.
-///
-/// # Example
-///
-/// ```
-/// # use wasm_react::{*, hooks::*};
-/// # struct MyComponent;
-/// impl Component for MyComponent {
-///   fn render(&self) -> VNode {
-///     let input_element = use_js_ref(None);
-///
-///     h!(div)
-///       .build(c![
-///         h!(input)
-///           .ref_container(&input_element)
-///           .html_type("text")
-///           .build(c![])
-///       ])
-///   }
-/// }
-/// ```
-pub fn use_js_ref<T: JsCast>(init: Option<T>) -> JsRefContainer<T> {
-  let ref_container = react_bindings::use_ref(
-    &init.map(|init| init.into()).unwrap_or(JsValue::null()),
-  );
-
-  JsRefContainer(ref_container, PhantomData)
 }
