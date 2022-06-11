9e45d5cba8a792c3a1524460e196776643928d20
Yichuan Shen <shen.yichuan@gmail.com>
2022-06-12 01:31:47 +0200

    Add card to material-ui example

--- examples/03-material-ui/js/mui-components.js
+++ examples/03-material-ui/js/mui-components.js
@@ -1,9 +1,11 @@
-export {
-  AppBar,
-  Box,
-  Toolbar,
-  Typography,
-  IconButton,
-  Button,
-} from "@mui/material";
-export { Menu as MenuIcon } from "@mui/icons-material";
+export { default as Box } from "@mui/material/Box";
+export { default as Container } from "@mui/material/Container";
+export { default as AppBar } from "@mui/material/AppBar";
+export { default as Toolbar } from "@mui/material/Toolbar";
+export { default as Typography } from "@mui/material/Typography";
+export { default as Card } from "@mui/material/Card";
+export { default as CardContent } from "@mui/material/CardContent";
+export { default as CardActions } from "@mui/material/CardActions";
+export { default as IconButton } from "@mui/material/IconButton";
+export { default as Button } from "@mui/material/Button";
+export { default as MenuIcon } from "@mui/icons-material/Menu";
--- examples/03-material-ui/src/lib.rs
+++ examples/03-material-ui/src/lib.rs
@@ -1,20 +1,21 @@
 use wasm_bindgen::prelude::*;
 use wasm_react::{
-  c, export_components, h, import_components,
+  c, export_components, import_components,
   props::{Props, Style},
   Component,
 };
 
 import_components! {
   #[wasm_bindgen(module = "/js/mui-components.js")]
-  AppBar, Toolbar, Typography, IconButton, Button, Box, MenuIcon
+  AppBar, Toolbar, Typography, IconButton, Button, Box as BoxComponent,
+  Container, Card, CardContent, CardActions, MenuIcon
 }
 
 pub struct App;
 
 impl Component for App {
   fn render(&self) -> wasm_react::VNode {
-    h!(div).build(c![
+    BoxComponent(&Props::new()).build(c![
       //
       AppBar(&Props::new()).build(c![
         //
@@ -35,10 +36,58 @@ impl Component for App {
               .insert("sx", Style::new().flex_grow(1).as_ref())
           )
           .build(c!["MUI Example Application"]),
-          //
-          Button(&Props::new().insert("color", &"inherit".into()))
-            .build(c!["Hello World!"])
         ]),
+      ]),
+      //
+      Container(
+        &Props::new().insert(
+          "sx",
+          &Style::new()
+            .margin_top(8)
+            .padding_top(2)
+            .padding_bottom(2)
+            .as_ref()
+        )
+      )
+      .build(c![
+        //
+        Card(
+          &Props::new()
+            .insert("variant", &"outlined".into())
+            .insert("sx", Style::new().max_width(345).as_ref())
+        )
+        .build(c![
+          //
+          CardContent(&Props::new()).build(c![
+            //
+            Typography(
+              &Props::new()
+                .insert("variant", &"h5".into())
+                .insert("component", &"h2".into())
+                .insert("sx", &Style::new().margin_bottom(1.5).as_ref())
+            )
+            .build(c!["Hello World!"]),
+            Typography(
+              //
+              &Props::new().insert("variant", &"body2".into())
+            )
+            .build(c![
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, ",
+              "sed do eiusmod tempor incididunt ut labore et dolore magna ",
+              "aliqua. Ut enim ad minim veniam, quis nostrud exercitation ",
+              "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ",
+              "aute irure dolor in reprehenderit in voluptate velit esse ",
+              "cillum dolore eu fugiat nulla pariatur. Excepteur sint ",
+              "occaecat cupidatat non proident, sunt in culpa qui officia ",
+              "deserunt mollit anim id est laborum."
+            ])
+          ]),
+          CardActions(&Props::new()).build(c![
+            //
+            Button(&Props::new().insert("size", &"small".into()))
+              .build(c!["Learn More"])
+          ])
+        ])
       ]),
     ])
   }
