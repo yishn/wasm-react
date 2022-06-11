33c0fba385dc99bf46a7a92e29933d8505a0fc27
Yichuan Shen <shen.yichuan@gmail.com>
2022-06-11 20:55:23 +0200

    Format with prettier

--- examples/03-material-ui/js/mui-components.js
+++ examples/03-material-ui/js/mui-components.js
@@ -1,2 +1,9 @@
-export { AppBar, Box, Toolbar, Typography, IconButton, Button } from "@mui/material";
+export {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+} from "@mui/material";
 export { Menu as MenuIcon } from "@mui/icons-material";
